9d513efb077ccf994eef9da2417688437095cd2b
MarucoMass <mariomass23@gmail.com>
2024-12-13 01:51:28 -0300

    added roles to user

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::post('auth/register', RegisterController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
